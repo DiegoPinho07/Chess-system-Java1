d32ff729b13efa18d1e1e6d2a609665899fbd759
Diego Pinho <diegoftpinho@gmail.com>
2024-08-26 20:20:35 -0300

    BoardExcetption e programação defensiva

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,12 +1,16 @@
 package boardgame;
 
-public class Board {
+public class Board  {
 
 	private int rows;
 	private int columns;
 	//matriz para construção do tabuleiro
 	private Piece[][] pieces;
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Erro ao criar o tabuleiro: Deve haver ao menos uma linha e uma coluna");
+		}
+	
 		this.rows = rows;
 		this.columns = columns;
 		//instanciando a classe piece incluindo como argumento as linhas e colunas
@@ -16,22 +20,24 @@ public class Board {
 	public int getRows() {
 		return rows;
 	}
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
+
 	public int getColumns() {
 		return columns;
 	}
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
 	//sobrecarga do método piece para retornar a posição da peça
-		public Piece piece(int row, int column) {
-			return pieces[row][column];
+	public Piece piece(int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Posição não existe no tabuleiro!!");
 			
 		}
+		return pieces[row][column];
+	}
+	
 		//método sobrecarregado para puxar os dados da classe board e retornar nessa classe.
 		public Piece piece(Position position) {
+			if (thereIsAPiece(position)) {
+				throw new BoardException("Já existe uma peça na posição " + position);
+			}
 			
 			return pieces[position.getRow()][position.getColumn()];
 		}
@@ -40,7 +46,16 @@ public class Board {
 			pieces[position.getRow()][position.getColumn()] = piece;
 			piece.position = position;
 		}
-		
+		private boolean positionExists(int row, int column) {
+			return row >=0 && row < rows && column >=0 && column < columns;
+		}
+		public boolean positionExists(Position position) {
+			return positionExists(position.getRow() , position.getColumn());
+		}
+		public boolean thereIsAPiece(Position position) {
+			return piece(position) != null;
+		}
 	}
+
 
 
